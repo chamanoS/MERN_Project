02d813f6afaf823b2d1009642be3c735f4c1e396
Chamano Ramashia <chamano.sr@gmail.com>
2021-04-05 09:36:29 +0200

    nav, footer, bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Container} from 'react-bootstrap'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 const App = ()=> {
   return (
     <div>
-      <h1>Welcome to Proshop</h1>
+      <Header></Header>
+      <main className="py-3">
+        <Container>
+        <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
